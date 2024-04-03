babc54f15232990f83961b3fba868c2ef70e6f52
danielznz <daniel.teixeira.corporativo@gmail.com>
2024-04-03 14:41:37 -0300

    js commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
+
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.titulo-projetos',{duration:2000,distance:'90px',delay: '150'})
+sr.reveal('.titulo-contato',{duration:2000,distance:'90px',delay: '150'})
+sr.reveal('#habilidades h1',{duration:2000,distance:'90px',delay: '150'})
